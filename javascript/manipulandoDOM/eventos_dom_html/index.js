4e004fbc31b4756b2c916e86105da22610485b9b
Edson Baierle <edsonbaierle@gmail.com>
2023-04-13 15:44:56 -0300

    Eventos no html
    Entendendo os eventos que ocorrem no html

---
+++ javascript/manipulandoDOM/eventos_dom_html/index.js
@@ -0,0 +1,23 @@
+function onMouseEnter(){
+  console.log("Entrada Do Mouse")
+}
+
+function onChange(){
+  console.log("Valor do elemento mudado!")
+}
+
+function onFocus(){
+  console.log("Foco no elemento!")
+}
+
+function onBlur(){
+  console.log("Desfocando do elemento!")
+}
+
+function onClick(){
+  console.log("Ao clicar no elemento!")
+}
+
+function onMouseOver(){
+  console.log("Passei por cima!!")
+}
