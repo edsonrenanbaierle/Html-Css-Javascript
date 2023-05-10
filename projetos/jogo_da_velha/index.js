efdff9946c9f2b421018271a5bf4cdc10fdd912f
Edson Baierle <edsonbaierle@gmail.com>
2023-05-10 16:22:56 -0300

    Modal
    Construído o modal do jogo da velha.

--- projetos/jogo_da_velha/index.js
+++ projetos/jogo_da_velha/index.js
@@ -1,17 +1,34 @@
-const openModal = document.querySelector('#open-modal')
-const closeModeal = document.querySelector('#start')
-const modal = document.querySelector('#modal')
-const fade = document.querySelector('#fade')
 
-const vet = [openModal, closeModeal, fade]
 
-function toogleModal(){
-  fade.classList.toggle('hide')
-  modal.classList.toggle('hide')
-}
+// Obtém o botão de abrir o modal e o modal
+const openModal = document.getElementById("open-modal");
+const modal = document.getElementById("modal");
+const fade = document.getElementById("fade")
 
-vet.forEach(function(ev){
-  ev.addEventListener('click', function(){
-    toogleModal()
-  })
-})
+// Obtém o botão de fechar o modal
+const closeModal = modal.querySelector("#close-modal");
+
+// Adiciona evento de clique ao botão de abrir o modal
+openModal.addEventListener("click", function() {
+  modal.style.display = 'block';
+  modal.style.position = 'fixed';
+  modal.style.top = '50%';
+  modal.style.left = '50%';
+  modal.style.transform = 'translate(-50%, -50%)';
+  modal.style.padding = '1rem';
+  modal.style.borderRadius = '0.5rem';
+  modal.style.display = 'grid';
+  modal.style.gridTemplateRows = '1fr';
+  modal.style.rowGap = '0.3rem';
+  modal.style.zIndex = '10';
+  modal.style.backgroundColor = 'var(--bg-color)';
+  modal.style.color = 'var(--font-color)';
+  fade.style.display = "block";
+});
+
+// Adiciona evento de clique ao botão de fechar o modal
+closeModal.addEventListener("click", function() {
+  modal.style.display = "none";
+  fade.style.display = "none";
+});
+
