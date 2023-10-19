d4356291ae0dc00bb563e2328a0c155534bd2ad5
Edson Baierle <edsonbaierle@gmail.com>
2023-10-19 15:25:15 -0300

    Utilizando await em uma function async

--- javascript/modulo5/promise_all/index.js
+++ javascript/modulo5/promise_all/index.js
@@ -1,27 +1,32 @@
-// function asyncSum(a, b) {
-//   return new Promise((resolve, reject) => {
-//       if (typeof a !== 'number' || typeof b !== 'number') {
-//           reject('arguments must be of type number')
-//       } else {
-//           resolve(a + b)
-//       }
-//   })
+// async function asyncSum(a, b) {
+//   if (typeof a !== 'number' || typeof b !== 'number') {
+//       return Promise.reject('arguments must be of type number')
+//   } else {
+//       return a + b
+//   }
 // }
 
-// function asyncSubtract(a, b) {
-//   return new Promise((resolve, reject) => {
-//       if (typeof a !== 'number' || typeof b !== 'number') {
-//           reject('arguments must be of type number')
-//       } else {
-//           resolve(a - b)
-//       }
-//   })
+// async function asyncSubtract(a, b) {
+//   if (typeof a !== 'number' || typeof b !== 'number') {
+//     return Promise.reject('arguments must be of type number')
+//   } else {
+//     return a-b
+//   }
 // }
 
-// const sum = asyncSum(57, 22)
-// const subtract = asyncSubtract(57, null)
+// async function execute(){
 
-// Promise.all([sum, subtract]).then((result) => console.log(result)).catch((err) => console.log(err))
+//   try {
+//     const result = await asyncSum(7, null)
+//     console.log(result)
+//     const result2 = await asyncSubtract(7, 2)
+//     console.log(result2)
+//   } catch (err) {
+//     console.log(err)
+//   }
+// }
+
+// execute()
 
 const number = [1, 2, null, 3, 4, 5, 6, 7, 8]
 
